a95474f88095057858cbeabdb5cd75ec9285cefc
mohsenil85 <mohsenil85@gmail.com>
2013-04-02 21:18:18 -0600

    strang

--- Graph.java
+++ Graph.java
@@ -1,26 +1,44 @@
 public class Graph {
 	private String vertices = "";
 	private int graphSize = 0;
+	private boolean oneOrZero = false;
+	private int[][] array;
+	
+	private int[][] initArray(int size) {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				array[i][j] = 0;
+			}
+		}
+		return array;
+	}
 
 	public void printGraph() {
+		array = initArray(graphSize);
+			System.out.print(" ");
 		for (int i = 0; i < graphSize; i++) {
-			System.out.print("  ");
+			System.out.print("   ");
 			System.out.print(vertices.charAt(i) + "  ");// top line
 
 		}
 		System.out.println();
 		System.out.print("  ");
 		for (int i = 0; i < graphSize; i++) {
-			System.out.print("====");// line of equals signs
+			System.out.print("======");// line of equals signs
 		}
 		System.out.println();
 		for (int j = 0; j < graphSize; j++) {// vertical edge
-			System.out.print(vertices.charAt(j) + "  ");
+			System.out.print(vertices.charAt(j) + "   ");
 
 			for (int k = 0; k < graphSize; k++) {// stuff inside chart
-				System.out.print(" x  ");
+				if (oneOrZero){
+				System.out.print(array[j][k]);
+				} else {
+				System.out.print("0");
+				}
+				System.out.print("     ");
 			}
-			System.out.println();
+			System.out.println("\n");
 
 		}
 		System.out.println();
@@ -33,7 +51,7 @@ public class Graph {
 	}
 
 	public void addEdge(int source, int destination) {
-
+		this.array[source][destination]++;
 	}
 
 	public void washall() {
--- washallApp.java
+++ washallApp.java
@@ -5,7 +5,18 @@ public class washallApp {
 		theGraph.addVertex('A'); 
 		theGraph.addVertex('B'); 
 		theGraph.addVertex('C');
+		theGraph.addVertex('D');
+		theGraph.addVertex('E');
+		
 		System.out.println("Original Adjacency Matrix");
+		
+		theGraph.addEdge(0, 2); 
+		theGraph.addEdge(1, 0); 
+		theGraph.addEdge(1, 4); 
+		theGraph.addEdge(3, 4); 
+		theGraph.addEdge(4, 2);
+		
 		theGraph.printGraph();
+		
 	}
 }
