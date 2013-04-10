5dda18735dfd5c8a4c4421ede152ee4367658e14
mohsenil85 <mohsenil85@gmail.com>
2013-04-10 16:01:05 -0600

    zzzzrrrwwwvvvvvvvwubwubwub

--- Graph.java
+++ Graph.java
@@ -1,22 +1,25 @@
+/*logan mohseni
+ * -01449704
+ * lab 6:  washall's algorithm
+ * the washall() method can find the transitive closure of a graph given an  2 demensional array of nodes
+ */
 public class Graph {
-	private Vertex[] vertices = new Vertex[30];
+	private Vertex[] vertices = new Vertex[30];//note the limit of 30 nodes in our graph.  
 	private int graphSize = 0;
-//	private boolean oneOrZero = false;
 	private int[][] array;
-	
-	
-	private class Vertex{
+
+	private class Vertex {// when i started this, i just had a string of chars to store the names.  but this way is more enterprisey
 		char name;
-		int index;
-		private Vertex(char name, int index){
-			this.name = name;
-			this.index = index;
-		}
 		
+		private Vertex(char name) {
+			this.name = name;
+		}
+
 	}
-	
+
 	public void printGraph() {
-			System.out.print(" ");
+		System.out.println();
+		System.out.print(" ");
 		for (int i = 0; i < graphSize; i++) {
 			System.out.print("   ");
 			System.out.print(vertices[i].name + "  ");// top line
@@ -40,46 +43,42 @@ public class Graph {
 		}
 		System.out.println();
 	}
-	
-	private void initArr(){
-		for(int i = 0; i< graphSize; i++){
-			for(int j = 0; j < graphSize; j++){
+
+	private void initArr() {//after we know the size of the array, we fill it with zeros, which increment nicerly than nulls
+		for (int i = 0; i < graphSize; i++) {
+			for (int j = 0; j < graphSize; j++) {
 				array[i][j] = 0;
 			}
 		}
 	}
 
 	public void addVertex(char name) {
-		Vertex aVertex = new Vertex(name, graphSize);
-		vertices[graphSize] = aVertex;
-		graphSize++;
-		array = new int[graphSize][graphSize];//don't actually know in advance so we just create a new arr each time we add a vertex.
-		initArr();
+		Vertex aVertex = new Vertex(name);//object orienteering is good, right?  not sure if it's easier to have
+		vertices[graphSize] = aVertex;//a special private class for verticies.  at least this way we can encapsulate their names with their values
+		graphSize++;//this variable is used everywhere in the code.  it controls pretty much every single loop in here
+		array = new int[graphSize][graphSize];// don't actually know in advance
+												// so we just create a new arr
+												// each time we add a vertex.
+		initArr();//fill the array with zeros
 	}
 
 	public void addEdge(int source, int destination) {
-		this.array[source][destination]++;
-	}
+		array[source][destination]++;//increment the zeros already exisiting in the array to reflect a new connection
+	}//note that this code will break the washall algo code if a person adds multiple connections.  its cray.
 
 	public void washall() {
-		for (int horiz = 0; horiz < graphSize; horiz++) {
-			for (int vert = 0; vert < graphSize; vert++) {
-				
-//					System.out.print("vert = "  + vert + "  ");
-//					System.out.println("horiz = " + horiz);
-				if (array[horiz][vert] == 1) {
-					for (int k = 0; k< graphSize; k++){
-						if (array[k][horiz] == 1){
-							array[k][vert]++;
-					System.out.println("special!! " + vertices[vert].name + " to " + vertices[k].name);
-					System.out.println("implies!! " + vertices[horiz].name + " to " + vertices[k].name);
-					
+		for (int i = 0; i < graphSize; i++) {//select a row...
+			for (int j = 0; j < graphSize; j++) {//in each column in each row, 
+				if (array[i][j] == 1) {//check to see if a connection exists
+					for (int k = 0; k < graphSize; k++) {//if a connection is detected,
+						if (array[k][i] == 1) {//check everthing that can be reached from that node
+							if (array[k][j] < 1){//and if a connection is not already present,
+								array[k][j]++;//register that connection
+							}
 						}
 					}
-					System.out.println(vertices[horiz].name + " to " + vertices[vert].name);
 				}
 			}
-
 		}
 	}
 }
--- washallApp.java
+++ washallApp.java
@@ -1,32 +1,36 @@
-
+/*
+ * logan mohseni
+ * -01449704
+ * lab6
+ * driver for washall's algorithm
+ */
 public class washallApp {
 	public static void main(String[] args) {
-		Graph theGraph = new Graph();
+		Graph theGraph = new Graph();// initizilize an instance of Graph
+		//add however many vertices.  note an upper limit of 30 
 		theGraph.addVertex('A'); 
 		theGraph.addVertex('B'); 
 		theGraph.addVertex('C');
 		theGraph.addVertex('D');
 		theGraph.addVertex('E');
 		
-		System.out.println("Original Adjacency Matrix");
-		
-//		theGraph.addEdge(0, 2); 
-//		theGraph.addEdge(1, 0); 
-//		theGraph.addEdge(1, 4); 
-//		theGraph.addEdge(3, 4); 
-//		theGraph.addEdge(4, 2);
-		
+		//add edges between various vertices
+		//in this case, we just connect each node to his neighbor
 		theGraph.addEdge(0, 1); 
 		theGraph.addEdge(1, 2); 
 		theGraph.addEdge(2, 3); 
 		theGraph.addEdge(3, 4); 
-		theGraph.addEdge(4, 0);
+		theGraph.addEdge(4, 0); 
 		
-		theGraph.printGraph();
+		System.out.println("Original Adjacency Matrix:  ");
 		
-		theGraph.washall();
+		theGraph.printGraph();//what the graph looks like now...
 		
-		theGraph.printGraph();
+		theGraph.washall();//run the algorithm
+		
+		System.out.println("Transitive Closure:  ");
+		
+		theGraph.printGraph();//what the graph looks like afterwards.  note that the original array is changed.
 		
 	}
 }
